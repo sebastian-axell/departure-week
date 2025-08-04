d42e0de2d038c162d0d3789152707836d8df82b1
Sebastian Axell <sebastian.axell98@gmail.com>
2025-08-04 11:50:02 +0100

    updated jumbotrron

--- dweek/src/components/JumboTron.tsx
+++ dweek/src/components/JumboTron.tsx
@@ -5,7 +5,7 @@ export default function JumboTron() {
         <div className="bg-gray-100 p-10 rounded-2xl shadow-lg text-center mx-auto mb-5 mt-2">
             <h1 className="text-4xl font-bold mb-4 text-gray-900">We going to Copenhagen!</h1>
             <p className="text-lg text-gray-700 mb-6">
-                Discover amazing features, connect with others, and explore new possibilities.
+                Here are the events before we go, as well as the stuff up for grabs. 
             </p>
         </div>
     )
