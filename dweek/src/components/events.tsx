707c290299c0ba73ac9d582a8b24833d3f395542
Sebastian Axell <sebastian.axell98@gmail.com>
2025-08-14 13:57:05 +0100

    added danish flag to last event

--- dweek/src/components/events.tsx
+++ dweek/src/components/events.tsx
@@ -23,7 +23,14 @@ export default function Events({ events }: { events: Event[] }) {
                 <div className="absolute top-0 left-0 w-1 bg-gray-900 h-full rounded"></div>
                 {allEvents.map((event: Event, index: number) => (
                     <div key={index} className="relative mb-10">
-                        <div className="absolute left-[-1.8rem] top-1.5 w-3 h-3 bg-yellow-300 rounded-full z-10 border-2 shadow" />
+                        {
+                            index === allEvents.length - 1 ?
+                                <div className="absolute left-[-1.75rem] top-1.5 w-3 h-3 rounded-full z-10 border-2 shadow">
+                                    <img src="/denmark.svg" className="scale-[140%]"/>
+                                </div>
+                                :
+                                <div className="absolute left-[-1.75rem] top-1.5 w-3 h-3 bg-yellow-300 rounded-full z-10 border-2 shadow" />
+                        }
                         <div className="ml-2">
                             <p className="text-sm text-gray-500 font-medium">{new Date(event.date).toLocaleDateString('en-US', {
                                 month: 'short',
